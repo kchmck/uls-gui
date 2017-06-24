97d403a0045a060a0e5e4aad600d18d8a4fecb93
Mick Koch <mick@kochm.co>
2017-06-24 08:41:08 -0400

    Remove url BASE constants

--- src/shared/urls.js
+++ src/shared/urls.js
@@ -1,15 +1,11 @@
-const REC_BASE = "http://wireless2.fcc.gov/UlsApp/UlsSearch/license.jsp";
-const LOC_BASE = "http://wireless2.fcc.gov/UlsApp/UlsSearch/licenseLocDetail.jsp";
-const FREQ_BASE = "http://wireless2.fcc.gov/UlsApp/UlsSearch/licenseFreqDetail.jsp";
-
 export function recUrl(rkey) {
-    return `${REC_BASE}?licKey=${rkey}`;
+    return `http://wireless2.fcc.gov/UlsApp/UlsSearch/license.jsp?licKey=${rkey}`;
 }
 
 export function locUrl(rkey, lkey) {
-    return `${LOC_BASE}?licKey=${rkey}&keyLoc=${lkey}`;
+    return `http://wireless2.fcc.gov/UlsApp/UlsSearch/licenseLocDetail.jsp?licKey=${rkey}&keyLoc=${lkey}`;
 }
 
 export function freqUrl(rkey, fkey) {
-    return `${FREQ_BASE}?licKey=${rkey}&keyFreq=${fkey}`;
+    return `http://wireless2.fcc.gov/UlsApp/UlsSearch/licenseFreqDetail.jsp?licKey=${rkey}&keyFreq=${fkey}`;
 }
