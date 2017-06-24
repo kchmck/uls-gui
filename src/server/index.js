91841eeba52ed3a80377562f316b931444367722
Mick Koch <mick@kochm.co>
2017-06-24 07:15:57 -0400

    Check for test run before starting server

--- src/server/index.js
+++ src/server/index.js
@@ -9,110 +9,116 @@ import serve from "koa-static";
 import sqlite3 from "sqlite3";
 import {ArgumentParser} from "argparse";
 
-let args = (function() {
-    let parser = new ArgumentParser();
+function initServer() {
+    let args = (function() {
+        let parser = new ArgumentParser();
 
-    parser.addArgument(["-d", "--db"], {
-        required: true,
-        help: "path to scrape-fcc sqlite database",
-    });
+        parser.addArgument(["-d", "--db"], {
+            required: true,
+            help: "path to scrape-fcc sqlite database",
+        });
 
-    return parser.parseArgs();
-})();
+        return parser.parseArgs();
+    })();
 
-let db = new sqlite3.Database(args.db, sqlite3.OPEN_READONLY);
-let app = koa();
+    let db = new sqlite3.Database(args.db, sqlite3.OPEN_READONLY);
+    let app = koa();
 
-app.use(handlebars());
+    app.use(handlebars());
 
-app.use(favicon("static/favicon.ico"));
+    app.use(favicon("static/favicon.ico"));
 
-app.use(mount("/static", serve("static")));
+    app.use(mount("/static", serve("static")));
 
-app.use(route.get("/api/records", function*() {
-    const QUERY = `
-        select records.rkey, callsign, title, eligibility,
-               locations.lkey, latitude, longitude,
-               frequencies.fkey, frequency, power,
-               emission
-        from records, recordDetails, frequencies, locations, emissions
-        where recordDetails.rkey = records.rkey and
-              locations.rkey = records.rkey and
-              frequencies.rkey = records.rkey and
-              frequencies.lkey = locations.lkey and
-              emissions.rkey = records.rkey and
-              emissions.fkey = frequencies.fkey
-        order by records.rkey, locations.lkey, frequencies.fkey
-    `;
+    app.use(route.get("/api/records", function*() {
+        const QUERY = `
+            select records.rkey, callsign, title, eligibility,
+                   locations.lkey, latitude, longitude,
+                   frequencies.fkey, frequency, power,
+                   emission
+            from records, recordDetails, frequencies, locations, emissions
+            where recordDetails.rkey = records.rkey and
+                  locations.rkey = records.rkey and
+                  frequencies.rkey = records.rkey and
+                  frequencies.lkey = locations.lkey and
+                  emissions.rkey = records.rkey and
+                  emissions.fkey = frequencies.fkey
+            order by records.rkey, locations.lkey, frequencies.fkey
+        `;
 
-    let names = yield new Promise((resolve, reject) => {
-        let locs = [];
+        let names = yield new Promise((resolve, reject) => {
+            let locs = [];
 
-        let prevLoc = null;
-        let prevFreq = null;
-        let loc = null;
-        let freq = null;
+            let prevLoc = null;
+            let prevFreq = null;
+            let loc = null;
+            let freq = null;
 
-        db.each(QUERY, (err, row) => {
-            if (err) {
-                return reject(err);
-            }
-
-            let {rkey, lkey, fkey} = row;
-
-            if (lkey !== prevLoc) {
-                prevLoc = lkey;
-                prevFreq = null;
-
-                let csIdx = row.title.indexOf(row.callsign);
-                let dashIdx = row.title.slice(csIdx).indexOf("-");
-
-                if (locs[lkey] && locs[lkey].rkey !== rkey) {
-                    throw new Error("lkey clash");
+            db.each(QUERY, (err, row) => {
+                if (err) {
+                    return reject(err);
                 }
 
-                loc = {
-                    rkey: rkey,
-                    lkey: lkey,
-                    callsign: row.callsign,
-                    desc: row.title.slice(csIdx + dashIdx + 2),
-                    elig: (row.eligibility || "").toUpperCase(),
-                    lat: row.latitude,
-                    lng: row.longitude,
-                    freqs: [],
-                };
+                let {rkey, lkey, fkey} = row;
 
-                locs.push(loc);
-            }
+                if (lkey !== prevLoc) {
+                    prevLoc = lkey;
+                    prevFreq = null;
 
-            if (fkey !== prevFreq) {
-                prevFreq = fkey;
+                    let csIdx = row.title.indexOf(row.callsign);
+                    let dashIdx = row.title.slice(csIdx).indexOf("-");
 
-                freq = {
-                    fkey: fkey,
-                    power: row.power,
-                    freq: row.frequency,
-                    emissions: new Set(),
-                };
+                    if (locs[lkey] && locs[lkey].rkey !== rkey) {
+                        throw new Error("lkey clash");
+                    }
 
-                loc.freqs.push(freq);
-            }
+                    loc = {
+                        rkey: rkey,
+                        lkey: lkey,
+                        callsign: row.callsign,
+                        desc: row.title.slice(csIdx + dashIdx + 2),
+                        elig: (row.eligibility || "").toUpperCase(),
+                        lat: row.latitude,
+                        lng: row.longitude,
+                        freqs: [],
+                    };
 
-            freq.emissions.add(row.emission);
-        }, err => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(locs);
-            }
+                    locs.push(loc);
+                }
+
+                if (fkey !== prevFreq) {
+                    prevFreq = fkey;
+
+                    freq = {
+                        fkey: fkey,
+                        power: row.power,
+                        freq: row.frequency,
+                        emissions: new Set(),
+                    };
+
+                    loc.freqs.push(freq);
+                }
+
+                freq.emissions.add(row.emission);
+            }, err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(locs);
+                }
+            });
         });
+
+        this.body = names;
+    }));
+
+    app.use(function*() {
+        yield this.render("index", {});
     });
 
-    this.body = names;
-}));
+    app.listen(3000);
+}
 
-app.use(function*() {
-    yield this.render("index", {});
-});
-
-app.listen(3000);
+if (process.env.NODE_ENV !== "test") {
+    initServer();
+}
