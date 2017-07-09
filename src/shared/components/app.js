5b9c6f6329bc239cf5a8db34d1aa033c460f6b1c
Mick Koch <mick@kochm.co>
2017-07-09 17:16:37 -0400

    Update to bootstrap v4alpha6

--- src/shared/components/app.js
+++ src/shared/components/app.js
@@ -114,11 +114,11 @@ const Emission = ({raw, em}) => h("li", {title: raw},
 const Freq = ({rkey, fkey, freq, power, rxPower, emissions}) => (
     h("div.freq", null, [
         h("div.row", null, [
-            h("h2.col-xs-10", null, h("a", {
+            h("h2.col-10", null, h("a", {
                 href: freqUrl(rkey, fkey),
                 title: `${freq} Hz`,
             }, `${dispFreq(freq)} MHz`)),
-            h("div.sReading.col-xs-2", {title: `${power / 1.0e3}W`},
+            h("div.sReading.col-2", {title: `${power / 1.0e3}W`},
                 calcSReading(rxPower)),
         ]),
         h("ul", null, emissions.map(raw =>
